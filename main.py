342c79b47e5537462bbabc5a5900de6825ab32c9
vexedmoth <vexedmoth@protonmail.com>
2024-08-20 15:38:01 +0200

    requirements.txt added

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 documents = SimpleDirectoryReader("data").load_data()
 
 # Embedding model
-Settings.embed_model = HuggingFaceEmbedding(model_name="all-MiniLM-L6-v2")
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 
 # LLM
 Settings.llm = Ollama(model="llama3.1:8b", request_timeout=360.0)
