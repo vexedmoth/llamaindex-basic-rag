6af72fa369f7d05ffc1c146a8f23bfb503a83dc6
vexedmoth <vexedmoth@protonmail.com>
2024-09-02 15:19:45 +0200

    update

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ Settings.llm = Ollama(model="llama3.1:8b", request_timeout=360.0)
 
 index = VectorStoreIndex.from_documents(documents)
 
-query_engine = index.as_query_engine()
+query_engine = index.as_query_engine(streaming=True)
+# streaming=True: Allows you to start printing or processing the beginning of the response before the full response is finished.
+
 response = query_engine.query("What did the author do growing up?")
-print(response)
+# print(response)
+response.print_response_stream()
